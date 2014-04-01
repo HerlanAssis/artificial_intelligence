7e0e767f6d1aaf7df99d65b08988dd4fcac51848
João Helis Bernardo <joaohelis.bernardo@gmail.com>
2014-03-31 22:01:58 -0300

    change n, in main, to 3

--- practices/pratice_03/npl.py
+++ practices/pratice_03/npl.py
@@ -63,7 +63,7 @@ def make_phrase(n, n_grams, words, _words_sequence_map):
 
 def main():
     words = bag_of_words('bag_of_words.txt')    
-    n = 1
+    n = 3
     n_grams = make_n_gram(n, words)        
     _words_sequence_map = words_sequence_map(n_grams)
     print 'Markov N-gram generation - Artificial Inteligence - João Helis\n'
