27eddbd6371305b52cca338ff7ee9e6edea79c60
João Helis Bernardo <joaohelis.bernardo@gmail.com>
2014-04-07 18:19:40 -0300

    mutation method added

--- practices/practice_04/genetic_algorithm.py
+++ practices/practice_04/genetic_algorithm.py
@@ -1,28 +1,31 @@
 import random
 
-def f(x): return abs(36 - sum(x))
+def f(individual):	
+	return abs(36 - sum([card + 1 for card in range(len(individual)) if individual[card]]))
 
-def make_population(number_of_individuals, cards, genes_per_individual, population=[]):	
+def make_population(number_of_individuals, population=[]):	
 	if number_of_individuals:
-		individual = []
-		while len(individual) < genes_per_individual:
-			genes = random.choice(cards)
-			if not(genes in individual): individual.append(genes)
+		individual = [0] * 10		
+		number_of_chosen_cards = random.randint(1, 10)
+		chosen_cards = []
+		while len(chosen_cards) < number_of_chosen_cards:
+			chosen_card = random.randint(1, 10)
+			if not(chosen_card in chosen_cards):
+				individual[chosen_card-1] = 1
+				chosen_cards.append(chosen_card)
 		population.append(individual)
-		return make_population(number_of_individuals-1, cards, genes_per_individual, population)
+		return make_population(number_of_individuals-1, population)
 	return population
 
 def evaluate_individuals(individuals):
 	return [(f(individual), individual) for individual in individuals]
 
-def uniform_crossover(parents):
-	while True:
-		children = [[],[]]
-		for i in range(len(parents[0])):
-			number_drawn = random.randint(0, 1)
-			children[0].append(parents[number_drawn][i])
-			children[1].append(parents[1 if number_drawn == 0 else 0][i])
-		if len(children[0]) == len(set(children[0])) and len(children[1]) == len(set(children[1])): break
+def uniform_crossover(parents):	
+	children = [[],[]]
+	for i in range(len(parents[0])):
+		drawn_number = random.randint(0, 1)
+		children[0].append(parents[drawn_number][i])
+		children[1].append(parents[1 if drawn_number == 0 else 0][i])		
 	return children
 
 def roulette(n, population):
@@ -36,33 +39,60 @@ def roulette(n, population):
 		init_interval = init_interval + individual[0]
 	while len(parents) < n:
 		random_number = random.random() * sum_evaluated_population
-		key = [key for key in interval_list_map.keys() if float(key[:key.find('_')]) <= random_number and random_number <float(key[key.find('_')+1:])][0]
-		if len(interval_list_map[key]) == len(set(interval_list_map[key])) and not(interval_list_map[key] in parents):
+		key = [key for key in interval_list_map.keys() if float(key[:key.find('_')]) <= random_number and random_number < float(key[key.find('_')+1:])][0]
+		if not(interval_list_map[key] in parents):
 			parents.append(interval_list_map[key])
 	return parents
 
-def genetic_algorithm(population, max_gerations, percent_of_mutation):
-	number_of_individuals_original_population = len(population)	
-	while max_gerations > 0:
+def mutate(individual, percent_of_mutate):
+	qtde_genes_for_mutate = int(len(individual) * percent_of_mutate)
+	indices_of_modified_genes = []	
+	while qtde_genes_for_mutate:
+		index_gene = random.randint(0, len(individual) -1)
+		if index_gene in indices_of_modified_genes: continue
+		individual[index_gene] = 0 if individual[index_gene] else 1
+		indices_of_modified_genes.append(index_gene)
+		qtde_genes_for_mutate-= 1	
+	return individual
+
+def mutate_all(individuals, percent_of_mutate):
+	return [mutate(individual, percent_of_mutate) for individual in individuals]
+
+def individual_to_cards(individual):
+	return [card + 1 for card in range(len(individual)) if individual[card]]
+
+def genetic_algorithm(population, max_generations, percent_of_mutate):
+	number_of_individuals_original_population = len(population)
+	mutate_over_generations = int(max_generations * 0.5)
+	generation = 1
+	while max_generations > 1:
 		evaluated_population = evaluate_individuals(population)
 		if number_of_individuals_original_population != len(population):
 			evaluated_population.sort()
 			evaluated_population =  evaluated_population[:number_of_individuals_original_population]
 			population = [individual[1] for individual in evaluated_population]		
-		for individual in evaluated_population:
-			if individual[0] == 0: return individual[1]
+		for individual in evaluated_population:	
+			if individual[0] == 0:
+				print 'generation:',generation,'-',			
+				return individual_to_cards(individual[1])				
 		parents = roulette(int(len(population)*0.2), population)
 		parents_pair = [[parents[i], parents[i+1]] for i in range(0, len(parents)-1, 2)]		
 		children = []
 		for parents_pair_element in parents_pair:
 			children += uniform_crossover(parents_pair_element)
+		if generation > mutate_over_generations:
+			children = mutate_all(children, percent_of_mutate)
 		population += children
-		max_gerations-= 1
-	return population[:number_of_individuals_original_population]
+		max_generations-= 1
+		generation += 1
+	print 'generation:',generation,'->',
+	return individual_to_cards(sorted(evaluate_individuals(population))[0][1])	
 
 def main():
-	cards = range(1, 11)
-	population = make_population(20, cards, 5)
-	print genetic_algorithm(population, 4, 5)	
-
+	count = 0		
+	population = make_population(number_of_individuals=20)	
+	while count < 20:
+		result = genetic_algorithm(population, max_generations=5, percent_of_mutate=0.2)	
+		print result		
+		count += 1
 main()
